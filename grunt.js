c0da0f80c57d60fb744d2e60dd3c31bedfd4564e
Charlie Szymanski <charles.szymanski@thomsonreuters.com>
2012-11-28 11:29:25 -0500

    some scripts we're going to use often. better grunt.js

--- grunt.js
+++ grunt.js
@@ -1,52 +1,47 @@
-module.exports = function(grunt) {
-
-    // Project configuration.
-    grunt.initConfig({
-        min: {
-            header_js: {
+var header_js = {
                 src: [
                     'js/vendor/jquery-1.8.3.min.js',
                     'js/vendor/modernizr-2.6.2.js'
                 ],
                 dest: 'js_compiled/app-header.min.js'
-            },
-            footer_js: {
+            }
+
+var footer_js = {
                 src: [
                     'js/vendor/underscore-min.js',
+                    'js/vendor/d3.v2.min.js',
+                    'js/vendor/jquery.tipsy.js',
+                    'js/vendor/tabletop.js',
                     'js/templates.js',
                     'js/app.js',
                     'js/plugins.js',
-                    'js/main.js'
+                    'js/income-chart/income-chart.js',
+                    'js/main.js',
+                    
                 ],
                 dest: 'js_compiled/app-footer.min.js'
             }
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        min: {
+            header_js: header_js,
+            footer_js: footer_js
         },
         concat: {
             css: {
                 src: [
                     'css/normalize.css',
+                    'css/tipsy.css',
                     'css/main.css',
                     'css/app.css'
                 ],
                 dest: 'css/app.min.css'
             },
-            header_dev_js: {
-                src: [
-                    'js/vendor/jquery-1.8.3.min.js',
-                    'js/vendor/modernizr-2.6.2.js'
-                ],
-                dest: 'js_compiled/app-header.js'
-            },
-            footer_dev_js: {
-                src: [
-                    'js/vendor/underscore-min.js',
-                    'js/templates.js',
-                    'js/app.js',
-                    'js/plugins.js',
-                    'js/main.js'
-                ],
-                dest: 'js_compiled/app-footer.js'
-            }
+            header_dev_js: header_js,
+            footer_dev_js: footer_js
         },
         less: {
             all: {
