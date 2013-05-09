2960e8da7573ecaa22c384712787b2eac57cbf04
Charlie Szymanski <charlie@charlieszymanski.com>
2013-05-09 14:01:55 -0400

    Modernized Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,21 @@
 var header_js_src = [
-                    'js/vendor/jquery.js',
-                    'js/vendor/modernizr.js'
-                ];
-                
+    'js/vendor/jquery.js',
+    'js/vendor/modernizr.js'
+];
+
 
 var footer_js_src = [
-                    'js/vendor/jquery.js', //redundant with header. For embedding.
-                    'js/vendor/underscore.js',
-                    'js/vendor/backbone.js',
-                    'js/vendor/d3.js',
-                    'js/vendor/jquery.tipsy.js',
-                    'js/vendor/tabletop.js',
-                    'js/closureStart.frag.js', // defines a closure that wraps the following...
-                    'js/templates.js',
-                    'js/plugins.js',
-                    'js/main.js',
-                    'js/closureEnd.frag.js' // ends closure, passes jquery (no conflict), _ (no conflict), backbone (no conflict), and d3.
-                ];
+    'js/vendor/jquery.js', //redundant with header. For embedding.
+    'js/vendor/underscore.js',
+    'js/vendor/backbone.js',
+    'js/vendor/d3.js',
+    'js/vendor/jquery.tipsy.js',
+    'js/closureStart.frag.js',  // defines a closure that wraps the following...
+    'js/templates.js',
+    'js/plugins.js',
+    'js/main.js',
+    'js/closureEnd.frag.js' 
+];
 
 module.exports = function(grunt) {
 
@@ -30,7 +29,7 @@ module.exports = function(grunt) {
             footer_js:{
                 src: 'js_compiled/app-footer.js',
                 dest: 'js_compiled/app-footer.min.js'
-            }
+            },
         },
         concat: {
             css: {
@@ -39,7 +38,7 @@ module.exports = function(grunt) {
                     'css/tipsy.css',
                     'css/cleanslate.css',
                     'css/main.css',
-                    'css/app.css'
+                    'css/less/!(reset).css',
                 ],
                 dest: 'css/app.min.css'
             },
@@ -50,13 +49,17 @@ module.exports = function(grunt) {
             footer_dev_js:{
                 src: footer_js_src,
                 dest: 'js_compiled/app-footer.js'
-            }
+            },
         },
         less: {
             all: {
-                files: {
-                    'css/app.css': 'less/*.less'
-                }
+                files: [{
+                    expand: true,
+                    cwd: 'less',
+                    src: ['**/*.less'],
+                    dest: 'css/less',
+                    ext: '.css',
+                }]
             },
         },
         jst: {
@@ -65,25 +68,40 @@ module.exports = function(grunt) {
                     processName: function(filename) {
                         return filename.split("/").pop().split(".")[0];
                     },
-                    namespace: 'Reuters.Graphics.Views'
+                    namespace: 'Reuters.Polls.Template'
                 },
                 files: {
                     'js/templates.js': ['jst/*.html']
                 }
+            },
+            individuals: {
+               options: {
+                    processName: function(filename) {
+                        return filename.split("/").pop().split(".")[0];
+                    },
+                    namespace: 'Reuters.Template'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'jst',
+                    src: ['**/*.html'],
+                    dest: 'js_compiled/templates',
+                    ext: '.js',
+                }]
             }
         },
         watch: {
             css: {
-                files: ['less/*.less'],
+                files: ['less/**/*.less'],
                 tasks: ['less', 'concat:css']
             },
             jst: {
-                files: ['jst/*.html'],
-                tasks: ['jst']
+                files: ['jst/**/*.html'],
+                tasks: ['jst', 'concat:footer_dev_js', 'uglify:footer_js']
             },
             js: {
-                files: ['js/*.js'],
-                tasks: ['concat:header_dev_js', 'concat:footer_dev_js', 'uglify:header_js', 'uglify:footer_js'],
+                files: ['js/**/*.js'],
+                tasks: [ 'concat:header_dev_js', 'concat:footer_dev_js', 'uglify:header_js', 'uglify:footer_js'],
             }
         }
     });
