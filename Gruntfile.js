4585ca9ef5bcd99c4caa0f14bae4a827228a7db5
Charlie Szymanski <charlie@charlieszymanski.com>
2013-06-05 10:05:59 -0400

    Added Grunt-S3 for uploads.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,7 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
+        aws: grunt.file.readJSON('grunt-aws.json'),
         uglify: {
             header_js: {
                 src: 'js_compiled/app-header.js',
@@ -103,7 +104,69 @@ module.exports = function(grunt) {
                 files: ['js/**/*.js'],
                 tasks: [ 'concat:header_dev_js', 'concat:footer_dev_js', 'uglify:header_js', 'uglify:footer_js'],
             }
+        },
+        
+        
+        s3: {   
+            dev:{
+                options:{
+                    key: '<%= aws.key %>',
+                    secret: '<%= aws.secret %>',
+                    bucket: '<%= aws.bucket %>',
+                    access: 'private',
+                    maxOperations: 20,
+                    //encodePaths: true,
+                    debug: false,
+                    gzip: true,
+                    gzipExclude: ['.png', '.jpg', '.gif', '.jpeg']
+                },
+                upload:[
+                    {
+                        src: 'js_compiled/*',
+                        dest: '<%= aws.folder %>js_compiled/',
+                    },
+                    {
+                        src: 'js_compiled/templates/*',
+                        dest: '<%= aws.folder %>js_compiled/templates/',
+                    },
+                    {
+                        src: 'js_compiled/templates/widgets/*',
+                        dest: '<%= aws.folder %>js_compiled/templates/widgets/',
+                    },
+                    {
+                        src: 'js_compiled/widgets/*',
+                        dest: '<%= aws.folder %>js_compiled/widgets/',
+                    },
+                    {
+                        src: 'css/*',
+                        dest: '<%= aws.folder %>css/',
+                    },
+                    {
+                        src: 'css/less/*',
+                        dest: '<%= aws.folder %>css/less/',
+                    },
+                    {
+                        src: 'css/less/widgets/*',
+                        dest: '<%= aws.folder %>css/less/widgets/',
+                    },
+                    {
+                        src: 'img/*',
+                        dest: '<%= aws.folder %>img/',
+                        gzip: false,
+                    },
+                    {
+                        src: 'assets/fonts/*',
+                        dest: '<%= aws.folder %>assets/fonts/',
+                        gzip: false,
+                    },
+                    {
+                        src: '*',
+                        dest: '<%= aws.folder %>',
+                    }
+                ]
+            }
         }
+        
     });
 
     grunt.loadNpmTasks('grunt-contrib-jst');
@@ -111,6 +174,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-s3');
 
     // Default task.
     grunt.registerTask('default', ['less', 'jst', 'concat', 'uglify']);
